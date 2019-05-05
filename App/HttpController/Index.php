e9bb2be5ae6530791169846d026789647784d885
yh <1104753531@qq.com>
2019-05-05 23:26:42 +0800

    .

--- App/HttpController/Index.php
+++ App/HttpController/Index.php
@@ -5,11 +5,24 @@ use EasySwoole\Http\AbstractInterface\Controller;
 
 class Index extends Controller
 {
-    public function index()
+    function index()
     {
-        $this->response()->write('hello world');
-        // TODO: Implement index() method.
+        $this->writeJson(200, [], 'success');
+        return '/test';
     }
+
+    function test()
+    {
+        $this->response()->write('this is test');
+        return '/test2';//当执行完test方法之后,返回/test2,让框架继续调度/test2方法
+    }
+
+    function test2()
+    {
+        $this->response()->write('this is test2');
+        return true;
+    }
+    
     public function say()
     {
         $this->response()->write('say hello world');
