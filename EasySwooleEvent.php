08b19614a8541c578f0ffe4af3a4748f0706feae
yehua <1104753531@qq.com>
2019-06-14 13:20:43 +0800

    Update EasySwooleEvent.php

--- EasySwooleEvent.php
+++ EasySwooleEvent.php
@@ -48,9 +48,9 @@ class EasySwooleEvent implements Event
         // TODO: Implement mainServerCreate() method.
         //在全局的主服务中创建事件中，实例化该Render,并注入你的驱动配置
         //在全局的主服务中创建事件中，实例化该Render,并注入你的驱动配置
-        Render::getInstance()->getConfig()>setRender(new Smarty());
+        Render::getInstance()->getConfig()->setRender(new \Smarty());
 //        Render::getInstance()->getConfig()>setRender(new \R());
-//        Render::getInstance()->getConfig()->setRender(new \SmartyRender('tmp'));
+        Render::getInstance()->getConfig()->setRender(new \SmartyRender('tmp'));
 //        $one_process = new \App\Components\Process\One("processName",time(),false,2,true);
 //        ServerManager::getInstance()->getSwooleServer()->addProcess($one_process->getProcess());
 
